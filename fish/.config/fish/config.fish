c25807759b37ced73c42a1ea61c8e812e4faca66
willstocks <will@willstocks.co.uk>
2025-08-07 09:55:50 +0100

    add fish config and increase hyprland scaling

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+starship init fish | source
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end

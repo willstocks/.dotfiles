195ac1da31f4ca1102e997151646387a53c98fe7
willstocks <will@willstocks.co.uk>
2026-01-03 20:10:50 +0000

    hyprland config change for new windowrule format

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 starship init fish | source
+zoxide init --cmd cd fish | source
 
 # overwrite greeting
 # potentially disabling fastfetch

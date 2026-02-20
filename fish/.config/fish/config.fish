932f2f1a3567061e7f7ec4f121e4e767d14620bd
willstocks <will@willstocks.co.uk>
2026-02-20 23:16:32 +0000

    Add lazygit alias for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 starship init fish | source
 zoxide init --cmd cd fish | source
 
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
